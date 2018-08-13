3597c72555417a3a7067667eae02eab4420c8090
Ryan Hord <ryhord13@gmail.com>
2018-08-13 16:38:35 -0400

    added sql db files

--- data.sql
+++ data.sql
@@ -1,1 +1,46 @@
 ﻿
+INSERT INTO users (userName, [password], userFirstName, userLastName, birthday, userAge, userHeight, userCurrentWeight, userDesiredWeight, recommendedDailyCaloricIntake, mealStreak, meal1, meal2, meal3, meal4, meal5, mealOrder)
+	VALUES ('jdean', 'iLoveFood', 'James', 'Dean', '19280810', 90, 72, 200, 180, 1500, 4, 1, 2, 3, 0, 0, '1,2,3');
+INSERT INTO users (userName, [password], userFirstName, userLastName, birthday, userAge, userHeight, userCurrentWeight, userDesiredWeight, recommendedDailyCaloricIntake, mealStreak, meal1, meal2, meal3, meal4, meal5, mealOrder)
+	VALUES ('revans', 'iLoveBreakfast', 'Robert', 'Evans', '19580707', 60, 65, 190, 170, 1600, 3, 1, 2, 0, 0, 0, '1,2');
+
+INSERT INTO foods (foodName, servingSize, calories, foodGroup) VALUES ('egg', 1, 78, 'protein');
+INSERT INTO foods (foodName, servingSize, calories, foodGroup) VALUES ('bacon', 1, 43, 'protein');
+INSERT INTO foods (foodName, servingSize, calories, foodGroup) VALUES ('sausage', 1, 229, 'protein');
+
+INSERT INTO meals (mealName) VALUES ('Breakfast');
+INSERT INTO meals (mealName) VALUES ('Lunch');
+INSERT INTO meals (mealName) VALUES ('Dinner');
+INSERT INTO meals (mealName) VALUES ('Snack');
+INSERT INTO meals (mealName) VALUES ('Dessert');
+
+INSERT INTO users_foods (userId, dateOfEntry, mealId, numberOfServings, foodId)
+	VALUES (1, '20180811', 1, 2, 1);
+INSERT INTO users_foods (userId, dateOfEntry, mealId, numberOfServings, foodId)
+	VALUES (1, '20180811', 1, 2, 2);
+INSERT INTO users_foods (userId, dateOfEntry, mealId, numberOfServings, foodId)
+	VALUES (2, '20180811', 1, 1, 1);
+INSERT INTO users_foods (userId, dateOfEntry, mealId, numberOfServings, foodId)
+	VALUES (2, '20180811', 1, 1, 3);
+INSERT INTO users_foods (userId, dateOfEntry, mealId, numberOfServings, foodId)
+	VALUES (1, '20180812', 1, 1, 1);
+INSERT INTO users_foods (userId, dateOfEntry, mealId, numberOfServings, foodId)
+	VALUES (1, '20180812', 1, 2, 2);
+INSERT INTO users_foods (userId, dateOfEntry, mealId, numberOfServings, foodId)
+	VALUES (2, '20180812', 1, 2, 1);
+INSERT INTO users_foods (userId, dateOfEntry, mealId, numberOfServings, foodId)
+	VALUES (2, '20180812', 1, 2, 3);
+
+INSERT INTO dailyWeight (userId, dateOfEntry, todaysWeight) VALUES (1, '20180811', 200);
+INSERT INTO dailyWeight (userId, dateOfEntry, todaysWeight) VALUES (1, '20180812', 198);
+INSERT INTO dailyWeight (userId, dateOfEntry, todaysWeight) VALUES (2, '20180811', 190);
+INSERT INTO dailyWeight (userId, dateOfEntry, todaysWeight) VALUES (2, '20180812', 189);
+
+INSERT INTO quickMeals (userId, foodId, numberOfServings, quickMealId, lastUsed)
+	VALUES (1, 1, 2, 1, '20180812')
+INSERT INTO quickMeals (userId, foodId, numberOfServings, quickMealId, lastUsed)
+	VALUES (1, 2, 2, 1, '20180812')
+INSERT INTO quickMeals (userId, foodId, numberOfServings, quickMealId, lastUsed)
+	VALUES (2, 1, 2, 1, '20180812')
+INSERT INTO quickMeals (userId, foodId, numberOfServings, quickMealId, lastUsed)
+	VALUES (2, 3, 2, 1, '20180812')
--- schema.sql
+++ schema.sql
@@ -3,31 +3,97 @@
 USE master;
 GO
 
--- Delete the DemoDB Database (IF EXISTS)
-IF EXISTS(select * from sys.databases where name='DemoDB')
-DROP DATABASE DemoDB;
+-- Delete the HealthTrackDB (IF EXISTS)
+IF EXISTS(select * from sys.databases where name='HealthTrackDB')
+DROP DATABASE HealthTrackDB;
 GO
 
--- Create a new DemoDB Database
-CREATE DATABASE DemoDB;
+-- Create a new HealthTrackDB
+CREATE DATABASE HealthTrackDB;
 GO
 
--- Switch to the DemoDB Database
-USE DemoDB
+-- Switch to the HealthTrackDB
+USE HealthTrackDB;
 GO
 
+-- Begin a TRANSACTION that must complete with no errors
 BEGIN TRANSACTION;
 
-CREATE TABLE users
-(
-	id			int			identity(1,1),
-	username	varchar(50)	not null,
-	password	varchar(50)	not null,
-	salt		varchar(50)	not null,
-	role		varchar(50)	default('user'),
+CREATE TABLE users (
+	userId integer							IDENTITY(1,1),
+	userName varchar(20)					not null,
+	[password] varchar(70)					not null,
+	userFirstName varchar(20)				not null,
+	userLastName varchar(20)				not null,
+	birthday date							not null,
+	userAge integer							not null,
+	userHeight integer						not null,
+	userCurrentWeight integer				not null,
+	userDesiredWeight integer				not null,
+	recommendedDailyCaloricIntake integer	not null,
+	mealStreak integer						not null,
+	meal1 integer							not null,
+	meal2 integer							not null,
+	meal3 integer							not null,
+	meal4 integer							not null,
+	meal5 integer							not null,
+	mealOrder varchar(20)					not null,
 
-	constraint pk_users primary key (id)
+	CONSTRAINT pk_users PRIMARY KEY (userId)
 );
 
+CREATE TABLE foods (
+	foodId integer							IDENTITY(1,1),
+	foodName varchar(50)					not null,
+	servingSize varchar(50)					not null,
+	calories integer						not null,
+	foodGroup varchar(50)					not null,
+
+	CONSTRAINT pk_foods PRIMARY KEY (foodId)
+);
+
+CREATE TABLE meals (
+	mealId integer							IDENTITY(1,1),
+	mealName varchar(50)					not null,
+
+	CONSTRAINT pk_meals PRIMARY KEY (mealId)
+);
+
+CREATE TABLE users_foods (
+	rowId integer							IDENTITY(1,1),
+	userId integer							not null,
+	dateOfEntry date						not null,
+	mealId integer							not null,
+	numberOfServings decimal				not null,
+	foodId integer							not null,
+
+	CONSTRAINT pk_users_foods PRIMARY KEY (rowId),
+    CONSTRAINT fk_users_foods_userId FOREIGN KEY (userId) REFERENCES users(userId),
+	CONSTRAINT fk_users_foods_foodId FOREIGN KEY (foodId) REFERENCES foods(foodId),
+	CONSTRAINT fk_users_foods_mealId FOREIGN KEY (mealId) REFERENCES meals(mealId)
+);
+
+CREATE TABLE dailyWeight (
+	rowId integer							IDENTITY(1,1),
+	userId integer							not null,
+	dateOfEntry date						not null,
+	todaysWeight integer					not null,
+
+	CONSTRAINT pk_dailyWeight PRIMARY KEY (rowId),
+	CONSTRAINT fk_dailyWeight_users FOREIGN KEY (userId) REFERENCES users(userId)
+);
+
+CREATE TABLE quickMeals (
+	rowId integer							IDENTITY(1,1),
+	quickMealId integer						not null,
+	userId integer							not null,
+	foodId integer							not null,
+	numberOfServings decimal				not null,
+	lastUsed datetime						not null,
+
+	CONSTRAINT pk_quickMeals PRIMARY KEY (rowId),
+	CONSTRAINT fk_quickMeals_userId FOREIGN KEY (userId) REFERENCES users(userId),
+	CONSTRAINT fk_quickMeals_foodId FOREIGN KEY (foodId) REFERENCES foods(foodId),
+);
 
 COMMIT TRANSACTION;
