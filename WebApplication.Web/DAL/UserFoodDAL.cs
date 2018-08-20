9b003acedf8afe0cdfaaf6755f9238a70ed25d92
Ryan Hord <ryhord13@gmail.com>
2018-08-20 11:48:28 -0400

    UserFoodDAL closer to complete save food function. UserFood Model created

--- WebApplication.Web/DAL/UserFoodDAL.cs
+++ WebApplication.Web/DAL/UserFoodDAL.cs
@@ -11,20 +11,62 @@ namespace WebApplication.Web.DAL
     {
 		private readonly string connectionString;
 
-		public User SaveItemToUserFoodLog(User user)
+		public UserFoodDAL(string connectionString)
+		{
+			this.connectionString = connectionString;
+		}
+
+		public User SaveItemToUserFoodLog(User user, FoodItem foodItem)
 		{
 			try
 			{
 				using (SqlConnection conn = new SqlConnection(connectionString))
 				{
 					conn.Open();
-					SqlCommand cmd = new SqlCommand("UPDATE users SET userName = @userName, userFirstName = @userFirstName, userLastName = @userLastName, userHeight = @userHeight, userCurrentWeight = @userCurrentWeight, userDesiredWeight = @userDesiredWeight, recommendedDailyCaloricIntake = @recommendedDailyCaloricIntake, password = @password, salt = @salt WHERE userId = @userId", conn);
-					cmd.Parameters.AddWithValue("@userName", user.Username);
+
+					SqlCommand cmd = new SqlCommand( "SELECT * FROM foods WHERE foodName = @foodName", conn);
+					cmd.Parameters.AddWithValue("@foodName", foodItem.foods[0].Name);
+
+					var resultReturned = cmd.ExecuteReader();
+
+					// Check if food is in already in food table -- by name
+					if (!resultReturned.Read())
+					{
+						// add the food to the table
+
+						conn.Open();
+						SqlCommand newFood = new SqlCommand($"INSERT INTO foods (foodName, servingSize, calories, foodGroup) VALUES (@foodName, @servingSize, @calories, @foodGroup);", conn);
+						newFood.Parameters.AddWithValue("@foodName", foodItem.foods[0].Name);
+						newFood.Parameters.AddWithValue("@servingSize", foodItem.foods[0].Name); //not right
+						newFood.Parameters.AddWithValue("@calories", foodItem.foods[0].nf_calories);
+						newFood.Parameters.AddWithValue("@foodGroup", foodItem.foods[0].Name); // not right
+
+						cmd.ExecuteNonQuery();
+
+					}
+
+					//just need to insert user food record
+					UserFood userFood = new UserFood();
+					userFood.UserId = user.Id;
+					userFood.DateOfEntry = DateTime.Now;
+					userFood.CaloriesPerServing = (float)foodItem.foods[0].nf_calories;
+					userFood.MealId = 1; //This is a placeholder for now
+					userFood.NumberOfServings = 1; // This needs to be user entered
+					userFood.ServingSize = 1; // this neeeds to be retrieved/passed from the initial search results
+					userFood.ServingUnit = "g"; // this neeeds to be retrieved/passed from the initial search results
+
+					SqlCommand newUserFood = new SqlCommand($"INSERT INTO users_foods (userId, dateOfEntry, mealId, numberOfServings, servingSize, servingUnit) VALUES (@foodName, @servingSize, @calories, @foodGroup, @servingSize, @servingUnit);", conn);
+					newUserFood.Parameters.AddWithValue("@userId", userFood.UserId);
+					newUserFood.Parameters.AddWithValue("@dateOfEntry", userFood.DateOfEntry);
+					newUserFood.Parameters.AddWithValue("@mealId", userFood.MealId);
+					newUserFood.Parameters.AddWithValue("@numberOfServings", userFood.NumberOfServings);
+					newUserFood.Parameters.AddWithValue("@servingSize", userFood.ServingSize);
+					newUserFood.Parameters.AddWithValue("@servingUnit", userFood.ServingUnit);
 
 
 					cmd.ExecuteNonQuery();
 
-					return;
+					return user;
 				}
 			}
 			catch (SqlException ex)
