e6395291044369fc1e59479fe8a209c42616ce40
Brian Boomer <brian.t.boomer@gmail.com>
2018-08-21 15:07:11 -0400

    Added method GetWeights

--- WebApplication.Web/DAL/IWeightDAL.cs
+++ WebApplication.Web/DAL/IWeightDAL.cs
@@ -1,7 +1,12 @@
-﻿namespace WebApplication.Web.Controllers
-{
-	public interface IWeightDAL
-	{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace WebApplication.Web.DAL
+{
+    public interface IWeightDAL
+    {
+		IList<int> GetWeights(DateTime startDate, DateTime endDate);
 	}
 }
--- WebApplication.Web/DAL/WeightDAL.cs
+++ WebApplication.Web/DAL/WeightDAL.cs
@@ -7,5 +7,18 @@ namespace WebApplication.Web.DAL
 {
     public class WeightDAL : IWeightDAL
     {
-    }
+		private readonly string connectionString;
+
+		public WeightDAL(string connectionString)
+		{
+			this.connectionString = connectionString;
+		}
+
+		public IList<int> GetWeights(DateTime startDate, DateTime endDate)
+		{
+			List<int> weightsList = new List<int>();
+
+			return weightsList;
+		}
+	}
 }
