cb922d2834f68889a8830d4dacd42e62baa1b81c
Daniel Medina <11746348+dwmedina@users.noreply.github.com>
2018-08-21 15:17:05 -0400

    Begin building WeightDAL w/queries

--- WebApplication.Web/DAL/WeightDAL.cs
+++ WebApplication.Web/DAL/WeightDAL.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication.Web.Models;
 
 namespace WebApplication.Web.DAL
 {
@@ -14,9 +16,33 @@ namespace WebApplication.Web.DAL
 			this.connectionString = connectionString;
 		}
 
-		public IList<int> GetWeights(DateTime startDate, DateTime endDate)
+		public IList<int> GetWeights(User user, DateTime startDate, DateTime endDate)
 		{
 			List<int> weightsList = new List<int>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(
+                        "SELECT * FROM users " +
+                        "INNER JOIN dailyWeight ON users.userId = dailyWeight.userId " +
+                        "WHERE users.userId = @userId " +
+                        "AND dailyWeight.dateOfEntry > @startDate " +
+                        "AND dailyWeight.dateOfEntry < @endDate;");
+
+                    cmd.Parameters.AddWithValue("@userId", user.Id);
+                    cmd.Parameters.AddWithValue("@startDate", startDate.ToShortDateString());
+                    cmd.Parameters.AddWithValue("@endDate", endDate.ToShortDateString());
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
 
 			return weightsList;
 		}
