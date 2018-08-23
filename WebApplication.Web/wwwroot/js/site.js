b55bc56e88bca27fefc5c038cc0bafdba0209ccb
Ryan Hord <ryhord13@gmail.com>
2018-08-23 16:13:34 -0400

    quick edit food menu looks better

--- WebApplication.Web/wwwroot/js/site.js
+++ WebApplication.Web/wwwroot/js/site.js
@@ -7,9 +7,33 @@ $(".edit-weight").click(editWeight);
 function editFood() {
     var parentForm = $(this).parent();
     $(this).remove();
-    parentForm.append("<label>Meal Classification</label><select name=\"mealId\"><option value=\"1\">Breakfast</option><option value=\"2\">Lunch</option><option value=\"3\">Dinner</option><option value=\"4\">Snack</option><option value=\"5\">Dessert</option></select>");
-    parentForm.append("<label>Number of Servings: </label><select name=\"numberOfServings\"><option value =\"1\">1</option><option value=\"2\">2</option><option value=\"3\">3</option><option value=\"4\">4</option><option value=\"5\">5</option><option value=\"6\">6</option><option value=\"7\">7</option><option value=\"8\">8</option><option value=\"9\">9</option><option value=\"10\">10</option></select > ");
-    parentForm.append("<button class=\"btn update-btn\" type=\"submit\" value=\"submit\">Update</button>")
+    parentForm.append("<button class=\"btn update-btn\" type=\"submit\" value=\"submit\">Update</button>");
+    parentForm.append("<tr class=\"edit-meal-props\"><td class=\"spacer\"></td><td><div class=\"updateFood-properties\">" +
+        "<div class=\"mealClass-field\">" +
+        "<label>Meal Classification</label>" +
+        "<select class=\"form-control\" name =\"mealId\">" +
+        "<option value=\"1\">Breakfast</option>" +
+        "<option value =\"2\">Lunch</option>" +
+        "<option value =\"3\">Dinner</option>" +
+        "<option value =\"4\">Snack</option>" +
+        "<option value =\"5\">Dessert</option>" +
+        "</select > "
+        + "</div >" +
+        "<div class=\"numberOfServings-field\">" +
+        "<label>Number of Servings: </label>" +
+        "<select class=\"form-control\" name=\"numberOfServings\">" +
+        "<option value =\"1\">1</option> " +
+        "<option value =\"2\">2</option> " +
+        "<option value =\"3\">3</option> " +
+        "<option value =\"4\">4</option> " +
+        "<option value =\"5\">5</option> " +
+        "<option value =\"6\">6</option> " +
+        "<option value =\"7\">7</option> " +
+        "<option value =\"8\">8</option> " +
+        "<option value =\"9\">9</option> " +
+        "<option value =\"10\">10</option>" +
+        "</select >" +
+        "</div></div></td></tr>");
     return false;
 }
 
